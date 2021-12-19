e593da8f814c84fe490b2a8d3052da9667dad5ca
Joker <Joker9166@outlook.com>
2021-12-19 23:00:14 +0800

    delete method getMethodCallSet()

--- src/main/java/org/joker/java/call/hierarchy/JavaParseUtil.java
+++ src/main/java/org/joker/java/call/hierarchy/JavaParseUtil.java
@@ -1,13 +1,6 @@
 package org.joker.java.call.hierarchy;
 
 import com.github.javaparser.ast.Node;
-import com.github.javaparser.ast.body.MethodDeclaration;
-import com.github.javaparser.ast.expr.MethodCallExpr;
-import com.github.javaparser.resolution.declarations.ResolvedMethodDeclaration;
-
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
 
 public class JavaParseUtil {
 
@@ -23,27 +16,6 @@ public class JavaParseUtil {
         } else {
             return getParentNode(parentNode, nodeType);
         }
-    }
-
-    public static Set<ResolvedMethodDeclaration> getMethodCallSet(List<MethodCallExpr> methodCallExprList, MethodDeclaration methodDeclaration) {
-        ResolvedMethodDeclaration srcMethod = methodDeclaration.resolve();
-        Set<ResolvedMethodDeclaration> dstMethodSet = new HashSet<>(2);
-
-        for (MethodCallExpr methodCallExpr : methodCallExprList) {
-            try {
-                ResolvedMethodDeclaration callMethod = methodCallExpr.resolve();
-                if (callMethod.getQualifiedSignature().equals(srcMethod.getQualifiedSignature())) {
-                    MethodDeclaration disMethod = getParentNode(methodCallExpr, MethodDeclaration.class);
-                    if (disMethod != null) {
-                        dstMethodSet.add(disMethod.resolve());
-                    }
-                }
-            } catch (Exception e) {
-                System.err.printf("resolve error: %s%n", methodCallExpr);
-            }
-        }
-
-        return dstMethodSet;
     }
 
 }
